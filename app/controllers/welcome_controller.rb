8e8cf48e0ccb04dcd0ddaf89650b10b29a8697a4
Maykon Souza <maykon.souza@hotmail.com.br>
2020-12-13 22:18:27 -0300

    Relatorio

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -27,7 +27,17 @@ class WelcomeController < ApplicationController
 
     @rtts_1=rtts
 
-    @data1 = [{1 => rtts[0], 2 => rtts[1], 3 => rtts[2], 4 => rtts[3], 5 => rtts[4]}, address, @latence1 ]
+
+
+    if @latence1 < 30
+      indicator = ["success", "Ótima!"]
+    elsif @latence1 > 30 and @latence1 < 100
+        indicator = ["primary", "Boa"]
+    else
+      indicator = ["danger","Ruim!"]
+    end
+
+    @data1 = [{1 => rtts[0], 2 => rtts[1], 3 => rtts[2], 4 => rtts[3], 5 => rtts[4]}, address, @latence1, indicator ]
 
     address = "www.facebook.com.br"
     rtts = []
@@ -37,9 +47,17 @@ class WelcomeController < ApplicationController
     end
     @latence2 = rtts.sum/rtts.length
 
-    @data2 = {1 => rtts[0], 2 => rtts[1], 3 => rtts[2], 4 => rtts[3], 5 => rtts[4]}
+    if @latence2 < 30
+      indicator = ["success", "Ótima!"]
+    elsif @latence2 > 30 and @latence2 < 100
+        indicator = ["primary", "Boa"]
+    else
+      indicator = ["danger","Ruim!"]
+    end
 
-    address = "www.youtube.com.br"
+    @data2 = [{1 => rtts[0], 2 => rtts[1], 3 => rtts[2], 4 => rtts[3], 5 => rtts[4]}, address, @latence2, indicator ]
+
+    address = "45.231.196.1"
     rtts = []
     for i in 1..5
       result = `ping -c 3 #{address} | tail -1| awk '{print $4}' | cut -d '/' -f 2`.to_i
@@ -47,10 +65,18 @@ class WelcomeController < ApplicationController
     end
     @latence3 = rtts.sum/rtts.length
 
-    @data3 = {1 => rtts[0], 2 => rtts[1], 3 => rtts[2], 4 => rtts[3], 5 => rtts[4]}
+    if @latence3 < 30
+      indicator = ["success", "Ótima!"]
+    elsif @latence3 > 30 and @latence3 < 100
+        indicator = ["primary", "Boa"]
+    else
+      indicator = ["danger","Ruim!"]
+    end
+
+    @data3 = [{1 => rtts[0], 2 => rtts[1], 3 => rtts[2], 4 => rtts[3], 5 => rtts[4]}, address, @latence3, indicator ]
     
 
-    address = "www.globo.com"
+    address = "phys.org"
     rtts = []
     for i in 1..5
       result = `ping -c 3 #{address} | tail -1| awk '{print $4}' | cut -d '/' -f 2`.to_i
@@ -58,7 +84,15 @@ class WelcomeController < ApplicationController
     end
     @latence4 = rtts.sum/rtts.length
 
-    @data4 = {1 => rtts[0], 2 => rtts[1], 3 => rtts[2], 4 => rtts[3], 5 => rtts[4]}
+    if @latence4 < 30
+      indicator = ["success", "Ótima!"]
+    elsif @latence4 > 30 and @latence4 < 100
+        indicator = ["primary", "Boa"]
+    else
+      indicator = ["danger","Ruim!"]
+    end
+
+    @data4 = [{1 => rtts[0], 2 => rtts[1], 3 => rtts[2], 4 => rtts[3], 5 => rtts[4]}, address, @latence4, indicator ]
 
 
 
